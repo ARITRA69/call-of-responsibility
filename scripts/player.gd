f66aa0adc7968076fff470165a66f34da21489a7
Genweb-maker <genweb037@gmail.com>
2025-10-22 10:38:18 +0530

    feat: added npc with code to move in randon direction . Isuse is that its nnot in the boundery .Need to update the code

--- scripts/player.gd
+++ scripts/player.gd
@@ -2,14 +2,18 @@ extends CharacterBody2D
 
 
 const SPEED = 500.0
+@onready var animation_player: AnimationPlayer = $AnimationPlayer
 
 func _enter_tree() -> void:
 	set_multiplayer_authority(name.to_int())
 
 func _physics_process(delta: float) -> void:
 	
-	if !is_multiplayer_authority(): return
+	#if !is_multiplayer_authority(): return
 	
 	velocity = Input.get_vector("move_left", "move_right", "move_up", "move_down") * SPEED
+	
+	if Input.is_action_just_pressed("attack"):
+		animation_player.play("Punch")
 	
 	move_and_slide()
